ec738d213873a6a241eb377d014170127fb17268
mglaer <87866626+mglaer@users.noreply.github.com>
2022-04-18 16:20:45 +0300

    lab2

---
+++ lab2/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/debug.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/debug.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/net_sockets.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/net_sockets.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_cache.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_cache.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_ciphersuites.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_ciphersuites.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_client.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_client.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_cookie.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_cookie.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_msg.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_msg.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_ticket.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_ticket.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls12_client.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls12_client.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls12_server.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls12_server.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls13_client.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls13_client.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls13_generic.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls13_generic.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls13_keys.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls13_keys.c.o"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library/ssl_tls13_server.c" "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedtls.dir/ssl_tls13_server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "mbedtls_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/include"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/mbedtls/library"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedx509.dir/DependInfo.cmake"
+  "C:/Users/mglaer/AndroidStudioProjects/libs/mbedtls/build/x86_64/library/CMakeFiles/mbedcrypto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/libs/mbedtls/build/x86_64/programs/pkey/CMakeFiles/pk_verify.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pk_verify.dir/pk_verify.c.o"
+  "pk_verify"
+  "pk_verify.manifest"
+  "pk_verify.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pk_verify.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
